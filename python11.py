0d6dee052845a20ccb11fc5ce672851b1b417720
Ruslan Namazov <r.namazov@corp.mail.ru>
2022-03-26 15:00:11 +0200

    Showing best practices Add solve function wrapper

--- python11.py
+++ python11.py
@@ -1,9 +1,12 @@
 На ввод даются два числа, на вывод сумма нечетных чисел в их промежутке.
 
-a = int(input())
-b = int(input())
-sum=0
-for i in range(a,b+1):
-    if (i%2==1):
-        sum+=i
-print(sum)
+func solve():
+ a = int(input())
+ b = int(input())
+ sum=0
+ for i in range(a,b+1):
+     if (i%2==1):
+         sum+=i
+ print(sum)
+
+solve()
